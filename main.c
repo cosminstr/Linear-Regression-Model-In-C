ec338df109f89e78bfcbc5a78d849d3fdf3e3bfa
Cosmin Stercu <cosminstercu@gmail.com>
2024-10-25 18:32:12 +0300

    added predict function

--- main.c
+++ main.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+void predict(int *predictions, int *hoursOfStudy, int size, int weight, int bias){
+
+    for(int i = 0; i <= size - 1; ++i){
+
+        *(predictions + i) = weight * (*(hoursOfStudy + i)) + bias;
+    }
+}
+
 int main(){
 
     // y = weight * x + bias
 
 
     //training data
-    int *x = (int *) malloc (5 * sizeof(int)); //independent variable
-    int *y = (int *) malloc (5 * sizeof(int)); //dependent variable
+    int *x = (int *) malloc (5 * sizeof(int)); //hours of study
+    int *y = (int *) malloc (5 * sizeof(int)); //grade received by the student
+
+    int *modelPredictions = (int *) malloc (5 * sizeof(int));
 
 
     int rn;
@@ -18,25 +28,24 @@ int main(){
 
     //populate the training dataset
     //the scenario is number of hours of study related to the grade the student got
-    for(int i = 1; i <= 5; i++){
+    for(int i = 0; i <= 4; i++){
         *(x + i) = 2 * i;
         
-        //adds randomness to the training data (a number between 0 and 15)
-        rn = rand() % 15;
+        //adds randomness to the training data (a number between 0 and 10)
+        rn = rand() % 10;
 
         if((10 * (2 * i) + rn) > 100){
             *(y + i) = 100;
         }
         else{
             *(y + i) = 10 * (2 * i) + rn;
+            // the model will 'learn' that the weight is 10 and the bias is an aggregate of all random values that 'rn' takes
         }
     }
 
-    for(int i = 1; i <= 5; i++){
-        printf("%d ", *(x + i));
-        printf("%d ", *(y + i));
-    }
-
+    // for(int i = 1; i <= 5; i++){
+    //     printf("%d %d\n", *(x + i), *(y + i));
+    // }
 
     int weight = 0;
     int bias = 0;
